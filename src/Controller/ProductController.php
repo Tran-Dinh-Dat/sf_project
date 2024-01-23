a3b5a0d5925fc819fa1bbc4f2fcf2788d5dcbe73
Tran-Dinh-Dat <45207367+Tran-Dinh-Dat@users.noreply.github.com>
2024-01-23 23:07:45 +0700

    crud auto

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,114 +3,79 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/product')]
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    #[Route('/', name: 'app_product_index', methods: ['GET'])]
     public function index(ProductRepository $productRepository): Response
     {
-        $products = $productRepository->findAll();
-        dd($products);
-
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $productRepository->findAll(),
         ]);
     }
 
-    #[Route('/product/create', name: 'create_product')]
-    public function createProduct(EntityManagerInterface $entityManager): Response
+    #[Route('/new', name: 'app_product_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(199999);
-        $product->setDescription('Description the product!!!');
-        $entityManager->persist($product);
-        $entityManager->flush();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
 
-        return new Response('Saved new product with id '.$product->getId());
-    }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($product);
+            $entityManager->flush();
 
-    #[Route('/product/{id}', name: 'show_product')]
-    public function show(EntityManagerInterface $entityManager, int $id): Response
-    {
-        $product = $entityManager->getRepository(Product::class)->find($id);
-
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id ' . $id
-            );
+            return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return new Response('Check out this greate product: ' . $product->getName());
-    }
-
-    #[Route('/product2/{id}', name: 'show2_product')]
-    public function show2(ProductRepository $productRepository, int $id): Response
-    {
-        $product = $productRepository->find($id);
-
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id ' . $id
-            );
-        }
-
-        return new Response('Check out this greate product: ' . $product->getName());
-    }
-
-    #[Route('/product3/{id}', name: 'show3_product')]
-    public function show3(Product $product): Response
-    {
-        return new Response('Check out this greate product: ' . $product->getName());
-    }
-
-    #[Route('/product/edit/{id}', name: 'update_product')]
-    public function update(EntityManagerInterface $entityManager, int $id): Response
-    {
-        $product = $entityManager->getRepository(Product::class)->find($id);
-
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id ' . $id
-            );
-        }
-
-        $product->setName('Keyboard updated');
-        $entityManager->flush();
-
-        return $this->redirectToRoute('show_product', [
-            'id' => $product->getId()
+        return $this->render('product/new.html.twig', [
+            'product' => $product,
+            'form' => $form,
         ]);
     }
 
-    #[Route('/product/delete/{id}', name: 'delete_product')]
-    public function delete(EntityManagerInterface $entityManager, int $id): Response
+    #[Route('/{id}', name: 'app_product_show', methods: ['GET'])]
+    public function show(Product $product): Response
     {
-        $product = $entityManager->getRepository(Product::class)->find($id);
-
-        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id ' . $id
-            );
-        }
-
-        $entityManager->remove($product);
-        $entityManager->flush();
-
-        return new Response('Deleted product ' . $product->getName());
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
     }
 
-    #[Route('/product-find', name: 'find_product')]
-    public function findAllGreaterThanPrice(EntityManagerInterface $entityManager): Response
+    #[Route('/{id}/edit', name: 'app_product_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Product $product, EntityManagerInterface $entityManager): Response
     {
-        $minPrice = 1000;
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
 
-        $products = $entityManager->getRepository(Product::class)->findAllGreaterThanPrice($minPrice);
-        dd($products);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_product_delete', methods: ['POST'])]
+    public function delete(Request $request, Product $product, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($product);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
     }
 }
